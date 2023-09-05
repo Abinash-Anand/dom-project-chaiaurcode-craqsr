e9fa21267998be95f24736bfd6c310f01ccdad3c
Abinash-Anand <74396889+Abinash-Anand@users.noreply.github.com>
2023-09-05 09:18:49 +0530

    1 file modified

--- 5-keyboard/chaiaurcode.js
+++ 5-keyboard/chaiaurcode.js
@@ -1,1 +1,23 @@
 console.log('Project 5');
+const keyId = document.querySelector('.key');
+document.body.addEventListener(
+  'keypress',
+  function (e) {
+    console.log(e.key);
+    console.log(e.code);
+    keyId.innerHTML = ` <table>
+    <tr>
+      <th>key</th>
+      <th>keyCode</th>
+      <th>code</th>
+    </tr>
+    <tr>
+      <td>${e.key === ' ' ? 'Space' : e.key}</td>
+      <td>${e.keyCode}</td>
+      <td>${e.code}</td>
+    </tr>
+   
+  </table> `;
+  },
+  false
+);
